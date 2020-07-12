747b2978147282ec93426fe25aa8e5e93861eba8
Iryna <43754665+isherep@users.noreply.github.com>
2020-07-12 13:59:45 -0700

    Update PathsWithGivenSum.java

--- src/Trees/PathsWithGivenSum.java
+++ src/Trees/PathsWithGivenSum.java
@@ -1,6 +1,7 @@
 package Trees;
 
 import java.util.ArrayList;
+
 /*
   Start from the root and branch left and right, computing the sum thus far on each path.
   When we find the sum, we print the current path.
@@ -17,29 +18,90 @@ import java.util.ArrayList;
  That is—rather than asking “does this node start a path with the sum?,”
  we ask “does this node complete a path with the sum?”
  */
-
+// Java program to print all paths with sum k.
 public class PathsWithGivenSum {
 
-    public void findSum(TreeNode head, int sum, ArrayList<Integer> buffer,
-                        int level) {
-        if (head == null) return;
-        int tmp = sum;
-        buffer.add(head.val);
-        for (int i = level; i >= 0; i--) {
-            tmp -= buffer.get(i);
-            if (tmp == 0) print(buffer, i, level);
-        }
-        ArrayList<Integer> c1 = (ArrayList<Integer>) buffer.clone();
-        ArrayList<Integer> c2 = (ArrayList<Integer>) buffer.clone();
-        findSum(head.left, sum, c1, level + 1);
-        findSum(head.right, sum, c2, level + 1);
-    }
 
-    public void print(ArrayList<Integer> buffer, int level, int i2) {
-        for (int i = level; i <= i2; i++) {
-            System.out.print(buffer.get(i) + " ");
+
+        //utility function to print contents of
+//a vector from index i to it's end
+        static void printVector( ArrayList<Integer> list, int i)
+        {
+            for (int j = i; j < list.size(); j++)
+                System.out.print( list.get(j) + " ");
+            System.out.println();
         }
-        System.out.println();
+
+        // binary tree node
+        static class Node
+        {
+            int data;
+            Node left,right;
+            Node(int x)
+            {
+                data = x;
+                left = right = null;
+            }
+        };
+        static ArrayList<Integer> path = new ArrayList<Integer>();
+
+        // This function prints all paths that have sum k
+        static void printKPathUtil(Node root, int k)
+        {
+            // empty node
+            if (root == null)
+                return;
+
+            // add current node to the path
+            path.add(root.data);
+
+            // check if there's any k sum path
+            // in the left sub-tree.
+            printKPathUtil(root.left, k);
+
+            // check if there's any k sum path
+            // in the right sub-tree.
+            printKPathUtil(root.right, k);
+
+            // check if there's any k sum path that
+            // terminates at this node
+            // Traverse the entire path as
+            // there can be negative elements too
+            int f = 0;
+            for (int j = path.size() - 1; j >= 0; j--)
+            {
+                f += path.get(j);
+
+                // If path sum is k, print the path
+                if (f == k)
+                    printVector(path, j);
+            }
+
+            // Remove the current element from the path
+            path.remove(path.size() - 1);
+        }
+
+        // A wrapper over printKPathUtil()
+        static void printKPath(Node root, int k)
+        {
+            path = new ArrayList<Integer>();
+            printKPathUtil(root, k);
+        }
+
+        public static void main(String[] args){
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(3);
+        root.left.left = new TreeNode(2);
+        root.left.right = new TreeNode(1);
+        root.left.right.left = new TreeNode(1);
+        root.right = new TreeNode(-1);
+        root.right.left = new TreeNode(4);
+        root.right.left.left = new TreeNode(1);
+        root.right.left.right = new TreeNode(2);
+        root.right.right = new TreeNode(5);
+        root.right.right.right = new TreeNode(2);
+
+
     }
 }
 
