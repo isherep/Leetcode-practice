a31209c88580b5b98cf05dc98a3af55fed858152
Iryna <43754665+isherep@users.noreply.github.com>
2020-08-12 08:55:25 -0700

    Create Permutations.java

---
+++ src/Permutations/Permutations.java
@@ -0,0 +1,40 @@
+package Permutations;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Permutations {
+    public List<List<Integer>> permute(int[] nums) {
+
+        List<List<Integer>> result = new ArrayList<>();
+        // turn nums into arraylist
+        ArrayList<Integer> numsList = new ArrayList<>();
+        for (int n : nums) {
+            numsList.add(n);
+        }
+        backtrack(nums.length, numsList, result, 0);
+
+        return result;
+    }
+
+    public void backtrack(int n, ArrayList<Integer> numsList, List<List<Integer>> result, int first) {
+        // if first went over each number in the list
+        // you need to clone the arraylist before adding, otherwise when you swap, all the added results will point to that swapped list, leaving you output full of same lists.
+        if (first == n) {
+            result.add(new ArrayList<Integer>(numsList));
+            // return;
+        }
+        // at this level here first doesn't change
+        for (int i = first; i < n; i++) {
+            // swap i and first
+            Collections.swap(numsList, i, first);
+            // now do the same for the rest of the array
+            // go in depth further
+            backtrack(n, numsList, result, first + 1);
+            Collections.swap(numsList, i, first);
+
+        }
+    }
+
+}
